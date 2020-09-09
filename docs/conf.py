b581b68738848f0383767be1c6ba5309d6f591ca
Tristan Salles <tristan.salles@sydney.edu.au>
2020-09-09 22:05:11 +1000

    add

--- docs/conf.py
+++ docs/conf.py
@@ -109,7 +109,7 @@ todo_include_todos = False
 html_theme = "sphinx_rtd_theme"
 
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_logo = "logo/logo.png"
+html_logo = "logo/logo1.png"
 html_theme_options = {
     "logo_only": True,
     "display_version": False,
